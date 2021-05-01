f08840039d0fc8921ef764c03964f0851d0fb8e1
AdamHamdi <prodev.webdesign@gmail.com>
2021-05-01 22:56:31 +0100

    second commit

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -5,12 +5,25 @@ import {MatSliderModule} from '@angular/material/slider';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatListModule} from '@angular/material/list';
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
 
 const materials =[
   MatSliderModule,
   MatButtonModule,
   MatIconModule,MatMenuModule,
-  MatCardModule
+  MatCardModule,MatDatepickerModule,
+  MatListModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatFormFieldModule,
+  MatNativeDateModule,
+  MatInputModule
 ];
 
 @NgModule({
